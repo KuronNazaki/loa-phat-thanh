bd1b196fdaae67a933f11839f7e95a789ba70cb0
Huynh Hoang Huy <kr.nzk2002@gmail.com>
2021-11-19 00:15:46 +0700

    Add background to Hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import NavBar from './NavBar.js';
+import background from '../images/hero-background.jpg';
 
 function Hero() {
 	return (
 		<div className="hero">
 			<div className="wrapper">
 				<NavBar />
+				<div className="hero-background">
+					<div className="hero-background-overlay"></div>
+					<img src={background} alt="Hero Background" />
+				</div>
 			</div>
 		</div>
 	);
