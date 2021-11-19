df6277abb98a12420fffe6fb53b56c311d3b0ea5
Huynh Hoang Huy <kr.nzk2002@gmail.com>
2021-11-19 13:31:49 +0700

    Finish Player part

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import muteVolume from '../images/volume-mute.png';
 
 function Player() {
+	let scaledMuteVolume = muteVolume + ' 2x';
+
 	return (
 		<div className="player">
 			<div className="wrapper">
@@ -9,6 +12,16 @@ function Player() {
 						<div className="player-header">
 							<p>Đang phát</p>
 							<h2>twenty one pilots - Smithereens (Official Audio)</h2>
+						</div>
+					</div>
+					<div className="player-video">
+						<div className="player-video-container">
+							<div className="video-frame">
+								<button className="volume-button">
+									<img srcSet={scaledMuteVolume} alt="Mute button" />
+								</button>
+							</div>
+							<div className="video-background"></div>
 						</div>
 					</div>
 				</div>
