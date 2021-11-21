e8e9cde885c1d22a8568174410589aa07d7b09f9
Huynh Hoang Huy <akirahuynh2002@gmail.com>
2021-11-21 19:58:36 +0700

    Add Loading component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
+import { useEffect, useState } from 'react';
 import Hero from './components/Hero.js';
 import NavBar from './components/NavBar.js';
 import Player from './components/Player.js';
 import Slider from './components/Slider.js';
 import Footer from './components/Footer.js';
+import Loading from './components/Loading.js';
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    document.fonts.ready.then(() => {
+      setIsLoaded(true);
+    });
+  });
+
   return (
     <>
-      <NavBar />
-      <Slider />
-      <Hero />
-      <Player />
-      <Footer />
+      <div className={`loading-container ${isLoaded ? 'hidden' : 'visible'}`}>
+        <Loading />
+      </div>
+
+      <div className={`main ${isLoaded ? 'visible' : 'hidden'}`}>
+        <NavBar />
+        <Slider />
+        <Hero />
+        <Player />
+        <Footer />
+      </div>
     </>
   );
 }
